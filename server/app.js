69fb1507f787b5f9bddf6f1d3b9d95fa1fcd0d12
itka0526 <itka0526@gmail.com>
2023-11-26 23:42:25 +0300

    init

--- server/app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-app.use(express.static(path.join(process.cwd(), "/public/dist")));
-
-app.listen("8080", () => console.log("--- START ---"));
