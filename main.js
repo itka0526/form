4529c8bb56afdfdaa1c408466ca059b3b193a011
Itgelt Gankhulug <82323359+itka0526@users.noreply.github.com>
2023-11-27 02:19:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+
 const NUMBER_OF_QUESTIONS = 6;
 const NUMBER_OF_QUESTIONS_TO_PASS = 5;
 const ANSWERS = ["hello_world", "zero_and_one", ".py", "print", "1024**2*8", "yesmaybe"];
@@ -13,7 +14,7 @@ function finish() {
     let correctlyAnswered = [];
     for (let i = 1; i < NUMBER_OF_QUESTIONS + 1; i++) {
         const element = document.getElementById(i + "-input");
-        for (let ca of ANSWERS) if (ca.includes(element.value)) correctlyAnswered.push(element.value);
+        for (let ca of ANSWERS) if (ca == element.value) correctlyAnswered.push(element.value);
     }
     if (correctlyAnswered.length >= NUMBER_OF_QUESTIONS_TO_PASS) {
         replaceForm();
